f402345b66ffb71ac2655047c3ab025cf630a1eb
leahgibson <ldgibson@rams.colostate.edu>
2025-07-23 05:58:22 -0600

    update to plots

--- base_analysis.py
+++ base_analysis.py
@@ -58,45 +58,63 @@ weekly_ae.append(winter_peak_df["pace"].mean() / winter_peak_df["hr"].mean())
 weekly_ae.append(baseline_df["pace"].mean() / baseline_df["hr"].mean())
 
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+fig, axes = plt.subplots(ncols=7, sharey=True, figsize=(16, 4), dpi=300)
 
-# Plot against baseline
-fig, axes = plt.subplots(ncols=7, sharey=True, figsize=(20, 5))
+all_handles = []
+all_labels = []
+
 for i, (week, group) in enumerate(runs_df.groupby("week")):
     weekly_ae.append(group["pace"].mean() / group["hr"].mean())
-    axes[i].scatter(
+
+    h1 = axes[i].scatter(
         winter_peak_df["hr"],
         winter_peak_df["pace"],
-        label="Winter Peak",
+        label="Winter Peak (no heat)",
         color="gray",
         marker="s",
         alpha=0.7,
     )
-
-    axes[i].scatter(
+    h2 = axes[i].scatter(
         baseline_df["hr"],
         baseline_df["pace"],
-        label="Baseline",
+        label="Baseline (no heat)",
         color="lightgray",
         marker="^",
         alpha=0.7,
     )
-
     axes[i].set_title(f"Week {i + 1}")
-    axes[i].scatter(
+    h3 = axes[i].scatter(
         group["hr"],
         group["pace"],
-        label="with heat",
+        label=f"Week {i + 1} (w/ heat)",
         color=colors[i],
         alpha=0.7,
     )
-    axes[i].legend(loc="lower left")
+
+    if i == 0:
+        all_handles.extend([h1, h2])
+        all_labels.extend(["Winter Peak", "Baseline"])
+
+    all_handles.append(h3)
+    all_labels.append(f"Week {i + 1} (w/ heat)")
+
     axes[i].set_xlim(115, 175)
     axes[i].set_ylim(8, 11.5)
 
 axes[0].set_ylabel("Pace (min/mi)")
-fig.supxlabel("Heart Rate (bpm)")
+axes[3].set_xlabel("Heart Rate (bpm)")
 
+plt.figlegend(
+    all_handles,
+    all_labels,
+    loc="lower center",
+    ncol=len(all_labels),
+    bbox_to_anchor=(0.5, 0.01),
+)
 plt.tight_layout()
+plt.subplots_adjust(bottom=0.2)
+plt.savefig("ae_scatter.png")
+exit()
 plt.show()
 
 # Plot weekly avg
